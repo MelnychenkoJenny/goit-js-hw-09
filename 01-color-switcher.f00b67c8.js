df4fe9aff679ee2a4931e600bab932680faab8e9
MelnychenkoJenny <107564673+MelnychenkoJenny@users.noreply.github.com>
2023-03-14 14:24:59 +0000

    Deploying to gh-pages from @ MelnychenkoJenny/goit-js-hw-09@8656592c9e8a81903cbc07d79c0c32173480e579 🚀

---
+++ 01-color-switcher.f00b67c8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){n=setInterval(o,1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(n),document.body.removeAttribute("style"),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.f00b67c8.js.map
