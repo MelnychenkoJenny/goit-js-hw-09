3e48022176bc51ca569b446fd0ac808a0c05f0fb
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-03-14 22:57:26 +0200

    css btn

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,23 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let intervalId = null;
-startBtn.addEventListener('click', onBackgroundColorChangeBtnClick);
-stopBtn.addEventListener('click', onStopBtnClick);
+startBtn.addEventListener('click', onStartColorChangeBtnClick);
+stopBtn.addEventListener('click', onStopColorChangeBtnClick);
 
-function onBackgroundColorChangeBtnClick() {
+function onStartColorChangeBtnClick() {
   intervalId = setInterval(changeBodyColor, 1000);
-  startBtn.setAttribute('disabled', '')
+  startBtn.setAttribute('disabled', '');
 }
 
-function onStopBtnClick() {
-    clearInterval(intervalId);
-    document.body.removeAttribute('style');
-    startBtn.removeAttribute('disabled')
+function onStopColorChangeBtnClick() {
+  clearInterval(intervalId);
+  document.body.removeAttribute('style');
+  startBtn.removeAttribute('disabled');
 }
-
 
 function changeBodyColor() {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }
+  document.body.style.backgroundColor = getRandomHexColor();
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
