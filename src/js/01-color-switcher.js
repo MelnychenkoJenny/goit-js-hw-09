8656592c9e8a81903cbc07d79c0c32173480e579
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-03-14 16:24:32 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId = null;
+startBtn.addEventListener('click', onBackgroundColorChangeBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
 
+function onBackgroundColorChangeBtnClick() {
+  intervalId = setInterval(changeBodyColor, 1000);
+  startBtn.setAttribute('disabled', '')
+}
+
+function onStopBtnClick() {
+    clearInterval(intervalId);
+    document.body.removeAttribute('style');
+    startBtn.removeAttribute('disabled')
+}
+
+
+function changeBodyColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
